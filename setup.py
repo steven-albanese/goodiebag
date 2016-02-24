c4deec5abdd07e1aa3f9c3d401442e0f7de8ae66
juliebehr <jul.marie.behr@gmail.com>
2016-02-24 13:42:48 -0500

    Module goodiebag

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='goodiebag',
+      version='0.1',
+      description='small molecule targeted kinase inhibitors',
+      url='https://github.com/juliebehr/goodiebag',
+      author='Julie M. Behr',
+      author_email='julie.behr@choderalab.org',
+      packages=['goodiebag'],
+      zip_safe=False)
