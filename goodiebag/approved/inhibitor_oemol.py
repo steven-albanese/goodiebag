70b18e84adc524e83a02217cceec57b8bfcf7467
juliebehr <jul.marie.behr@gmail.com>
2016-02-24 22:27:41 -0500

    ApprovedOEMolGenerator class to turn csv entries into OEMols

--- goodiebag/approved/inhibitor_oemol.py
+++ goodiebag/approved/inhibitor_oemol.py
@@ -24,12 +24,105 @@ class ApprovedOEMolGenerator(object):
     """
     def __init__(self):
         self.inhibitor_smiles = dict()
+        self.inhibitors = list()
+        self._kinases = dict()
         with open('clinical-kinase-inhibitors.csv', 'r') as csvi:
             inhibitor_reader = csv.reader(csvi)
-            for inhibitor in inhibitor_reader:
-                self.inhibitor_smiles[inhibitor[0]] = inhibitor[1]
+            for inhibitor_line in inhibitor_reader:
+                drug_name = inhibitor_line[0]
+                smiles = inhibitor_line[1]
+                kinase_list = inhibitor_line[2].split(' ')
+                self.inhibitor_smiles[drug_name] = smiles
+                self.add_inhibitor(drug_name, smiles, kinase_list)
 
-    def generate_molecule(self, inhibitor_name):
-        mol_smiles = self.inhibitor_smiles[inhibitor_name]
+    def generate_molecule(self, drug_name):
+        if drug_name not in self.inhibitor_names:
+            raise KeyError('Inhibitor called %s is not recognized' % drug_name)
+        mol_smiles = self.inhibitor_smiles[drug_name]
         return generate_OE_molecule(mol_smiles)
+
+    def add_inhibitor(self, drug_name, smiles, kinase_list):
+        inhibitor = Inhibitor(drug_name, smiles, kinase_list, self)
+        self.inhibitors.append(inhibitor)
+
+    def add_kinase(self, kinase_name):
+        kinase = Kinase(kinase_name, self)
+        self._kinases[kinase.name] = kinase
+
+    @property
+    def kinase_names(self):
+        return self._kinases.keys()
+
+    @property
+    def kinases(self):
+        return self._kinases.values()
+
+    @property
+    def kinase_by_name(self, name):
+        return self._kinases[name]
+
+    @property
+    def inhibitor_names(self):
+        return [inhibitor.name for inhibitor in self.inhibitors]
+
+class Inhibitor(object):
+    """
+    Arguments:
+    ----------
+        name : string
+        smiles : string
+        kinase_names : list of strings
+        dealer : ApprovedOEMolGenerator
+    """
+    def __init__(self, name, smiles, kinase_names, dealer):
+        self.name = name
+        self.smiles = smiles
+        self.dealer = dealer
+        self.kinases = list()
+        for kinase_name in kinase_names:
+            if kinase_name in dealer.kinase_names:
+                kinase = dealer.kinase_by_name(kinase_name)
+            else:
+                kinase = dealer.add_kinase(kinase_name)
+            self.kinases.append(kinase)
+            kinase.add_inhibitor(self)
+
+    def generate_molecule(self):
+        return self.dealer.generate_molecule(self.name)
+
+    @property
+    def kinase_names(self):
+        return [kinase.name for kinase in self.kinases]
+
+class Kinase(object):
+    """
+    Arguments:
+    ----------
+        name : string
+        dealer : ApprovedOEMolGenerator
+    """
+    def __init__(self, name, dealer):
+        self.name = name
+        self.inhibitors = list()
+        self.dealer = dealer
+
+    def add_inhibitor(self, inhibitor):
+        self.inhibitors.append(inhibitor)
+
+    @property
+    def inhibitor_names(self):
+        return [inhibitor.name for inhibitor in self.inhibitors]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
