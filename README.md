a5b9d4c8268acbeae96745d80c84ca083b9b1296
juliebehr <jul.marie.behr@gmail.com>
2016-02-25 22:38:38 -0500

    Maybe improving setup.py - not tested

--- README.md
+++ README.md
@@ -6,7 +6,5 @@ includes:
 * list of approved inhibitors (31 as of 25Jan2016);
 * file containing smiles strings;
 * select inhibitors still in trials;
-* images of structures;
-* simlinks between kinases and approved inhibitors
 
 
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
+VERSION = 0.1
+__version__ = VERSION
+
 setup(name='goodiebag',
-      version='0.1',
+      version=__version__,
       description='small molecule targeted kinase inhibitors',
       url='https://github.com/juliebehr/goodiebag',
       author='Julie M. Behr',
       author_email='julie.behr@choderalab.org',
-      packages=['goodiebag'],
-      zip_safe=False)
+      packages=['goodiebag','goodiebag.approved','goodiebag.intrials','goodiebag.kinases','goodiebag.tests'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose']
+)
